5ae6327bfa9adc747b430d0ece0fa7ca83e9c72c
wwsh <tommix3@gmail.com>
2021-06-20 22:05:29 +0200

    added more functional tests to cover update, delete endpoints

--- tests/Functional/Controller/ItemControllerTest.php
+++ tests/Functional/Controller/ItemControllerTest.php
@@ -36,4 +36,79 @@ class ItemControllerTest extends WebTestCase
         $item = $itemRepository->findOneByData($data);
         $this->assertEquals($user->getUsername(), $item->getUser()->getUsername());
     }
+
+    public function testUpdate(): void
+    {
+        $client = static::createClient();
+
+        $userRepository = static::$container->get(UserRepository::class);
+        $itemRepository = static::$container->get(ItemRepository::class);
+
+        /** @var User $user */
+        $user = $userRepository->findOneByUsername('john');
+
+        $client->loginUser($user);
+
+        $data = 'super secure cool data';
+
+        $newItemData = ['data' => $data];
+
+        $client->request('POST', '/item', $newItemData);
+        $this->assertResponseIsSuccessful();
+
+        $item = $itemRepository->findOneByData($data);
+        $this->assertEquals($user->getUsername(), $item->getUser()->getUsername());
+        $id = $item->getId();
+
+        $data = 'changed my mind with secure data';
+
+        $updatedItemData = ['data' => $data];
+
+        $client->request('PUT', '/item/' . $id, $updatedItemData);
+        $this->assertResponseIsSuccessful();
+
+        $client->request('GET', '/item');
+        $this->assertResponseIsSuccessful();
+
+        $this->assertStringContainsString('changed my mind with secure data', $client->getResponse()->getContent());
+        $this->assertStringNotContainsString('super secure cool data', $client->getResponse()->getContent());
+
+        $item = $itemRepository->findOneByData($data);
+        $this->assertEquals($user->getUsername(), $item->getUser()->getUsername());
+    }
+
+    public function testDelete(): void
+    {
+        $client = static::createClient();
+
+        $userRepository = static::$container->get(UserRepository::class);
+        $itemRepository = static::$container->get(ItemRepository::class);
+
+        /** @var User $user */
+        $user = $userRepository->findOneByUsername('john');
+
+        $client->loginUser($user);
+
+        $data = 'volatile data';
+
+        $newItemData = ['data' => $data];
+
+        $client->request('POST', '/item', $newItemData);
+        $this->assertResponseIsSuccessful();
+
+        $item = $itemRepository->findOneByData($data);
+        $this->assertEquals($user->getUsername(), $item->getUser()->getUsername());
+        $id = $item->getId();
+
+        $client->request('DELETE', '/item/' . $id);
+        $this->assertResponseIsSuccessful();
+
+        $client->request('GET', '/item');
+        $this->assertResponseIsSuccessful();
+
+        $this->assertStringNotContainsString('volatile data', $client->getResponse()->getContent());
+
+        $item = $itemRepository->findOneByData($data);
+        $this->assertNull($item);
+    }
 }
