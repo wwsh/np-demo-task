22f0d0fbaeb30ee80a9a3c04e9e9be308cb0904b
wwsh <tommix3@gmail.com>
2021-06-20 22:44:14 +0200

    added unit test cases for all methods in ItemService

--- tests/unit/Service/ItemServiceTest.php
+++ tests/unit/Service/ItemServiceTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\Unit\Service;
 use App\Entity\Item;
 use App\Entity\User;
 use App\Service\ItemService;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use PHPUnit\Framework\TestCase;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -42,5 +43,60 @@ class ItemServiceTest extends TestCase
         $this->entityManager->expects($this->once())->method('persist')->with($expectedObject);
 
         $this->itemService->create($user, $data);
+    }
+
+    public function testUpdate(): void
+    {
+        $data = 'update data';
+        $id = 1;
+        $entity = new Item();
+
+        $entity->setData($data);
+
+        $repositoryMock = $this->createMock(ServiceEntityRepository::class);
+        $repositoryMock
+            ->expects($this->once())
+            ->method('find')
+            ->with($id)
+            ->willReturn($entity);
+
+        $this->entityManager->expects($this->at(0))
+            ->method('getRepository')
+            ->with(Item::class)
+            ->willReturn($repositoryMock);
+
+        $this->entityManager->expects($this->at(1))
+            ->method('persist')
+            ->with($entity);
+
+        $this->itemService->update($id, $data);
+    }
+
+    public function testDelete(): void
+    {
+        $item = new Item();
+
+        $this->entityManager->expects($this->once())->method('remove')->with($item);
+
+        $this->itemService->delete($item);
+    }
+
+    public function testGetAll(): void
+    {
+        $user = $this->createMock(User::class);
+
+        $repositoryMock = $this->createMock(ServiceEntityRepository::class);
+        $repositoryMock
+            ->expects($this->once())
+            ->method('findBy')
+            ->with(['user' => $user])
+            ->willReturn([]);
+
+        $this->entityManager->expects($this->once())
+            ->method('getRepository')
+            ->with(Item::class)
+            ->willReturn($repositoryMock);
+
+        $this->itemService->getAll($user);
     }
 }
